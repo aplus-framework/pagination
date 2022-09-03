f5e572260d10d1f9690f369deea2fe1db797ce9f
mehran <mehran.taheri.t@gmail.com>
2022-09-03 09:12:25 +0430

    add persian language

---
+++ src/Languages/fa/pagination.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * This file is part of Aplus Framework Pagination Library.
+ *
+ * (c) Mehran Taheri <mehran.taheri.t@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+return [
+    'first' => 'اول',
+    'last' => 'آخر',
+    'next' => 'بعد',
+    'previous' => 'قبل',
+];
